017eaa9a089464607520075d6a2160b4930ea7a5
Dejaayyy <149858996+Dejaayyy@users.noreply.github.com>
2024-02-20 04:33:33 +0800

    Delete Hearth of Fortune directory

--- Hearth of Fortune/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $hostName = "localhost";
-    $dbUser = "root";
-    $dbPassword = "";
-    $dbName = "hof_db";
-    $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
-    if (!$conn) {
-        die("Something went wrong!");
-    }
